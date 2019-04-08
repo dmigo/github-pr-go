7c381bf7ef85af2fc6804171fe379920158fa4ce
dmigo <d.f.goryunov@gmail.com>
2019-04-08 08:25:47 +0200

    List commits

--- src/prs.go
+++ src/prs.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/google/go-github/github"
 )
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
+
+type PullRequestCommit struct {
+	SHA         *string         `json:"sha,omitempty"`
+	NodeID      *string         `json:"node_id,omitempty"`
+	Commit      *github.Commit  `json:"commit,omitempty"`
+	Author      *github.User    `json:"author,omitempty"`
+	Committer   *github.User    `json:"committer,omitempty"`
+	Parents     []github.Commit `json:"parents,omitempty"`
+	HTMLURL     *string         `json:"html_url,omitempty"`
+	URL         *string         `json:"url,omitempty"`
+	CommentsURL *string         `json:"comments_url,omitempty"`
+}
+
+func ListCommits(pullRequest *github.PullRequest) ([]*PullRequestCommit, error) {
+	var commits []*PullRequestCommit
+	r, err := httpClient.Get(*pullRequest.URL)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	decodeErr := json.NewDecoder(r.Body).Decode(&commits)
+
+	return commits, decodeErr
+}
 
 func main() {
 	ctx := context.Background()
@@ -18,13 +48,22 @@ func main() {
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	} else {
-		fmt.Println("Pull Requests:")
+		fmt.Println("pull requests:")
 		fmt.Println()
 		for index, pr := range prs {
 			fmt.Printf("%v.\t[%v] %v\n", index, *pr.ID, *pr.Title)
 			fmt.Printf("\t%v\n", *pr.URL)
-			if pr.Commits != nil {
-				fmt.Printf("\t[%v] %v\n", *pr.Commits, *pr.CommitsURL)
+
+			commits, err := ListCommits(pr)
+			if err != nil {
+				fmt.Printf("\t\terror while fetching commits: %v\n", err)
+			} else {
+				if pr.Commits != nil {
+					fmt.Printf("\t\tcommits(%v):\n", *pr.Commits)
+					for _, commit := range commits {
+						fmt.Printf("\t\t[%v] %v\n", *commit.SHA, *commit.URL)
+					}
+				}
 			}
 			fmt.Printf("\tby %v\n", *pr.User.Login)
 			fmt.Println()
